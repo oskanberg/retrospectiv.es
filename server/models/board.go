bc30b86fcd9c63fd1719ac09bbe9f8ecb0e2c93e
Oliver Skanberg-Tippen <oskanberg@andigital.com>
2016-07-25 11:45:08 +0100

    Add dummy board.

--- server/models/board.go
+++ server/models/board.go
@@ -11,17 +11,21 @@ type Board struct {
 	ID string `json:"id"`
 }
 
-type Boards map[string]Board
+type Boards map[string]ItemBoard
 
-func (bs *Boards) GetBoard(id string) (ItemBoard, bool) {
-	board, ok := (*bs)[id]
+func (bs Boards) GetBoard(id string) (ItemBoard, bool) {
+	board, ok := bs[id]
 	return board, ok
 }
 
-func NewItemBoard() ItemBoard {
-	return &Board{}
+func NewItemBoard(id string) ItemBoard {
+	return &Board{
+		ID: id,
+	}
 }
 
 func NewItemBoards() ItemBoards {
-	return &Boards{}
+	board := Boards{}
+	board["123"] = NewItemBoard("123")
+	return board
 }
